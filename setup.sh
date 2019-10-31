ee7fffd5f30ed02ff967e83d28bc1798baffe102
Ricardo Restituyo <rrestituyo@vivial.net>
2019-10-31 09:22:06 -0400

    Add i3blocks submodule

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/bash
+#!/bin/sh
 
-mkdir -p ~/.config/i3blocks/
 mkdir -p ~/.config/dunst/
-ln -sf `pwd`/i3blocks.conf ~/.config/i3blocks/config
+ln -sf `pwd`/i3blocks ~/.config/i3blocks
 ln -sf `pwd`/dunstrc ~/.config/dunst/dunstrc
