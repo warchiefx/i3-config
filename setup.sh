a7809fa0d91d15c3dd2e9d2e0573a3ea4b9c9b8a
Ricardo Restituyo <rrestituyo@vivial.net>
2019-11-01 14:35:35 -0400

    Add keychain integration

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@
 mkdir -p ~/.config/dunst/
 ln -sf `pwd`/i3blocks ~/.config/i3blocks
 ln -sf `pwd`/dunstrc ~/.config/dunst/dunstrc
+
+mkdir -p ~/.config/systemd/user/
+ln -sf `pwd`/systemd/keychain.service ~/.config/systemd/user/keychain.service
+systemctl --user enable keychain.service
